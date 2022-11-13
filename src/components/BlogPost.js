c2a0863115009b2db7905319e25ce38723ae44c6
MJ_Shelton <mjshelton12@comcast.net>
2022-11-12 23:37:40 -0800

    Refractoring

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,10 +1,14 @@
-// import { blogData } from '../data/blogData';
+var parse = require('html-react-parser')
 
 export default function BlogPost({setArticle, article}) {
-    // const list = blogData.map((blog) => <Post key={blog.id} post={blog}/>);
+    var blog = parse(article.object.blog)
+
     return(
-        <div className='project-container'>
+        <div>
             <p>{article.object.title}</p>
+            <div>
+                <p>{blog}</p>
+            </div>
             <button href="#" onClick={() => {
         setArticle({title: 'Blog', object: {}})
         }}>Return to Blog List</button>
--- src/data/blogData.js
+++ src/data/blogData.js
@@ -3,7 +3,7 @@ export const blogData = [
     id: 1,
     img: "https://i.ytimg.com/vi/5vHXcuceYcU/maxresdefault.jpg",
     alt: "MJ On Set",
-    title: "How is TV Like a Tech Project?",
+    title: "How is TV Post Production Like an Agile Project?",
     description:
       "A TV show is just a giant project. Don't believe me? Read this post to find out!",
     blog: `<p>Paragraph 1</p>
