8915ea91dd4a090a97d95af6e59b0c1f7ebfebfe
MJ_Shelton <mjshelton12@comcast.net>
2023-01-08 22:30:38 -0600

    Menu bug fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect }  from 'react'
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 
-import Navigation2 from './components/Navigation2'
+import Navigation2 from './components/Navigation'
 import Header from './components/Header'
 import Core from './components/Core'
 import Footer from './components/Footer'
