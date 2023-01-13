bb58cf0ae07de24d7bb9b7af5ef69d56a45e959e
MJ_Shelton <mjshelton12@comcast.net>
2023-01-12 22:40:40 -0600

    Refactoring

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,6 @@ export default function About() {
     return(
         <div className="about mb-32 lg:mb-0" data-aos="fade">
                 <div className="flex sm:row content-center items-center">
-                    <div></div>
                     <div className="flex items-center"><p className="aboutPara text-sm leading-snug">Michelle "MJ" Shelton started her career in television production offices where she <strong>led teams on multi-million dollar projects</strong> with such household names as <strong>Netflix, Apple,</strong> and <strong>Disney.</strong> Thanks to creative problem solving (and a little Googling), MJ found herself frequently responsible for <strong>IT management</strong> including IT purchases, set-up of network and digital filing systems, and in-house tech-support. After realizing this skill had developed into a passion, MJ enrolled in the <strong> Northwestern Coding Bootcamp</strong>  so she could take her technical skills to a new level across industries. Friendly, driven, and a creative problem-solver, MJ loves to be a part of a team working hard to create an efficient, effective project.</p></div>
                     <div className="flex items-center"><img id="about" src={aboutData.img} alt={aboutData.alt} /></div>
                 </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,40 +1,6 @@
 export default function Project({ project }) {
   return (
     <>
-      {/* <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white m-2">
-        <a href={project.site} target="blank">
-          <img
-            class="w-full"
-            src={project.img}
-            alt={project.title}
-            style={{ objectFit: "cover", height: "225px" }}
-          />{" "}
-        </a>
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{project.title}</div>
-          <div className="flex">
-            <button
-              className="bg-gray-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full"
-              href={project.site}
-              target="blank"
-            >
-              Live Site
-            </button>
-            <a href={project.repo} target="blank">
-              <img
-                className="github hover:bg-blue-400"
-                src="https://i.imgur.com/RQf6YHc.jpg"
-                alt={project.title}
-              />
-            </a>
-          </div>
-          <p>{project.description}</p>
-          <p className="text-sm mt-2">
-            <b className="text-gray-700 font-semibold">Technology Used:</b>{" "}
-            {project.tech}
-          </p>
-        </div>
-      </div> */}
       <div
         className="project-box m-2"
         style={{
